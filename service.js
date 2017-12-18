b2359c3d485d86c84721db07f541801fcfa38f22
Pasquale Boemio <pasquale.boemio@statwolf.com>
2017-12-18 14:06:26 +0000

    added service installer

--- service.js
+++ service.js
@@ -1,15 +1,20 @@
 const Service = require('node-windows').Service;
 const path = require('path');
 
+const script = path.join(__dirname, 'index.js');
+
+console.log(`Installing service ${ script }`);
+
 const svc = new Service({
   name: 'Node Message Broker',
   description: 'The Statwolf message broker',
-  script: path.join(__dirname, 'index.js'),
+  script,
   env: {
 	  name: 'PORT',
 	  value: '9999'
   }
 });
 
+svc.uninstall();
 svc.install();
 svc.start();
