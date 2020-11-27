5fc6685f1b36db930e933e1a9ecdd763167faa3e
Pasquale Boemio <boemianrapsodi@gmail.com>
2020-11-27 13:46:40 +0000

    updated depes

--- src/http.js
+++ src/http.js
@@ -2,7 +2,13 @@
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.json());
+
+app.use('/', function(req, res) {
+  res.json({
+    message: 'Hello from message broker'
+  });
+});
 
 const server = app.listen(9999, function() {
   console.log('Running message broker on http://0.0.0.0:9999');
